3afd2d4cfe7c703d3e713eb574e918d98674b9d9
Geoff Hudik <GeoffH@magenic.com>
2018-11-07 07:48:03 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Steeltoe.CloudFoundry.Connector.RabbitMQ;
+using Steeltoe.Extensions.Configuration.CloudFoundry;
+
+namespace river_flow_processor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddCloudFoundry();
+            var configuration = builder.Build();
+
+            var services = new ServiceCollection();
+            services.AddRabbitMQConnection(configuration);
+            var serviceProvider = services.BuildServiceProvider();
+
+            var queueConnFactory = serviceProvider.GetService<ConnectionFactory>();
+            const string queueName = "river-flow";
+
+            using (var queueConn = queueConnFactory.CreateConnection())
+            using (var queueChannel = queueConn.CreateModel())
+            {
+                queueChannel.QueueDeclare(
+                    queue: queueName, 
+                    durable: true, 
+                    exclusive: false, 
+                    autoDelete: false, 
+                    arguments: null);
+
+                var queueConsumer = new EventingBasicConsumer(queueChannel);
+                queueConsumer.Received += (model, ea) =>
+                {
+                    var json = Encoding.UTF8.GetString(ea.Body);
+                    Console.WriteLine($"Received river flow request:{Environment.NewLine}{json}");
+                };
+
+                queueChannel.BasicConsume(
+                    queue: queueName, 
+                    autoAck: true,
+                    consumer: queueConsumer);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# PCF River Flow Processor
+Simple sandbox app evaulating scaling a .NET Core console app with PCF according to queue length.
