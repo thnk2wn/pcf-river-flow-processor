2900fbff64ee240ed2444a6f406e2b6b39e66402
Geoff Hudik <GeoffH@magenic.com>
2019-05-19 15:28:37 -0700

    run prep script tweaks

--- scripts/run-prep.ps1
+++ scripts/run-prep.ps1
@@ -6,23 +6,28 @@ Push-Location $PSScriptRoot
 "Launching Eureka startup in another process"
 Invoke-Expression 'cmd /c start powershell -NoProfile -Command { .\eureka-start.ps1 }'
 
+""
 # Start Spring Cloud Config server for Producer and Consumer configuration.
 .\docker-cloud-config-start.ps1
 
+""
 # Start RabbitMQ container for Producer and Consumer
 .\docker-rabbitmq-start.ps1
 
+""
 # Start MySQL container for API. Mount to keep any existing data and not rebuild DB.
 .\docker-mysql-start.ps1 -mount
 
+""
 # Setup queue and seed with message(s) for consumer processing.
 .\producer-init.ps1 -all:$all -top:$top
 
+""
 # See if Eureka has fully started up yet, if not wait.
 .\eureka-wait.ps1
 
 "Run prep complete`n"
 
-docker ps -a
+docker ps -a --format 'table {{.Names}}\t{{.Image}}\t{{.Ports}}'
 
 Pop-Location
